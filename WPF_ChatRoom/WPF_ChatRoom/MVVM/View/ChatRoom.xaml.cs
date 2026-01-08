f51f08e3fbc62730d64440210480d40abc84f9c3
LAPTOP-21O86I06\LZM <1104407395@qq.com>
2026-01-08 18:20:35 +0800

    新增右框并绑定资源，新增名片资源字典，新增Model(Contact和Message),新建ViewModel(ChatRoomViewModel),建立MVVM模式

--- WPF_ChatRoom/WPF_ChatRoom/MVVM/View/ChatRoom.xaml.cs
+++ WPF_ChatRoom/WPF_ChatRoom/MVVM/View/ChatRoom.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPF_ChatRoom.MVVM.ViewModel;
 
 namespace WPF_ChatRoom.MVVM.View
 {
@@ -22,6 +23,7 @@ namespace WPF_ChatRoom.MVVM.View
         public ChatRoom()
         {
             InitializeComponent();
+            this.DataContext = new ChatRoomViewModel();
         }
 
 
---
+++ WPF_ChatRoom/WPF_ChatRoom/MVVM/ViewModel/ChatRoomViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_ChatRoom.MVVM.Model;
+
+namespace WPF_ChatRoom.MVVM.ViewModel
+{
+    public class ChatRoomViewModel
+    {
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        public ChatRoomViewModel()
+        {
+            Contacts = new ObservableCollection<ContactModel>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Alex{i}",
+                    ImageSource = "https://tse1.explicit.bing.net/th/id/OIP.BzjY_OsxeGhvVgd-4uP1cAHaE7?rs=1&pid=ImgDetMain&o=7&rm=3",
+                    Messages = new ObservableCollection<MessageModel> { new MessageModel
+                    {
+                        Username = $"Alex{i}",
+                        UsernameColor = "#409aff",
+                        ImageSource = "https://tse1.explicit.bing.net/th/id/OIP.BzjY_OsxeGhvVgd-4uP1cAHaE7?rs=1&pid=ImgDetMain&o=7&rm=3",
+                        Message = "first",
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true,
+                    } }
+                });
+            }
+        }
+    }
+}
